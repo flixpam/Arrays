b4c0b5b8ae48619f9ba0e50edd71a0270188beda
Marcos Cuenca <flixpam@gmail.com>
2016-04-21 12:45:21 +0200

    Añadiendo una primera modificacion al archivo orginal

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,7 @@
 public class Arrays{
   public static void main (String[] args){
     System.out.println("Vamos a empezar con los Arrays");
+    int [] prueba = {1,2,3,4,5,6,7,8,9,};
+    System.out.println(prueba[5]);
   }
 }  
